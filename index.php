999096fe16a26429ddba9b81e6739fb337dd6e17
antorocca <rocca83910@gmail.com>
2021-10-05 17:05:09 +0200

    1st commit
    1st page ok

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>CV Roccasalva Antony</title>
+        <link rel="icon" href="./Assets/Pictures/logo_rocca.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap" rel="stylesheet">
+        <link href="./Assets/Css/Index.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="div_logo">
+            <a href="index.php"><img class="logo_AR" src="assets/Pictures/Svg/logo_rocca_white3.png"      alt="logo Antony Roccasalva"></a>
+        </div>
+        <div class="fixContainer">
+            <a href="Assets/Html/presentation.html">
+            <div class="presentation"><h2>présentation</h2></div>
+            </a>
+        </div>
+        <div class="grid-CD">
+            <div class="fixContainerC">
+                <a href="Assets/Html/competence.html">
+                    <div class="competence"><h2>compétences</h2></div>
+                </a>
+            </div>
+            <div class="fixContainerD">
+                <a href="Assets/Html/divers.html">
+                    <div class="divers"><h2>divers</h2></div>
+                </a>
+            </div>
+        </div>
+        <footer>
+                <p>Antony Roccasalva</p>
+                <p>roccasalva.antony@gmail.com</p>
+                <p>06.09.30.83.43</p>
+        </footer>
+    </body>
+    </html>
